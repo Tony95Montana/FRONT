92b31b2eb5b10948ca7005f6f2ff706d3ca71a49
Tony Montana <theo.angoulvent95@gmail.com>
2025-01-13 11:07:04 +0100

    init style

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 window.onload = init;
 
-function init() {}
+function init() {
+    const Tweet = document.getElementById('Tweet');
+    Tweet.focus();
+}
+function exe() {
+    const Tweet = document.getElementById('Tweet');
+    const topic = document.getElementById('topic');
+    console.log(Tweet.value);
+    console.log(topic.value);
+}
