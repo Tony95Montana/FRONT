cccd2b7ecb04390086bc35c8623101c620307849
Tony Montana <theo.angoulvent95@gmail.com>
2025-01-13 11:43:17 +0100

    first prototype

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ function init() {
 function exe() {
     const Tweet = document.getElementById('Tweet');
     const topic = document.getElementById('topic');
-    console.log(Tweet.value);
-    console.log(topic.value);
+    if (Tweet.value == '' || topic.value == '') {
+        const x = document.getElementById("snackbar");
+        x.className = "show";
+        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    } else {
+        const load = document.getElementById('load');
+        load.classList.remove('d-none');
+        setTimeout(() => {
+            console.log(Tweet.value);
+            console.log(topic.value);
+            load.classList.add('d-none');
+        }, "3000");
+    }
 }
